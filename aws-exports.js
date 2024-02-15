3606f8f68017126c0108922c6f5f3c2affbea363
Vidvi <rikuvidqvist@hotmail.com>
2024-02-15 19:19:41 +0200

    added amplify authentication

---
+++ aws-exports.js
@@ -0,0 +1,6 @@
+// replace the user pool region, id, and app client id details
+export default {
+    "REGION": "eu-north-1",
+    "USER_POOL_ID": "eu-north-1_uESVzkBDS",
+    "USER_POOL_APP_CLIENT_ID": "5p0al8rl21rofjc4p4u1unt3r1"
+}
