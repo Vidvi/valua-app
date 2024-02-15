f9f29a77a559777e785ff9542d6daa4e81bff818
Vidvi <rikuvidqvist@hotmail.com>
2024-02-15 20:27:09 +0200

    fixing aws amplify and init backend

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css'
 
-//Amplify config
-
-Amplify.configure({
-  Auth: {
-    region: awsExports.REGION,
-    userPoolId: awsExports.USER_POOL_ID,
-    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
-  }
-})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Authenticator>
-          {({ signOut, user}) => (
-            <div>
-              <p>Welcome {user.username}</p>
-              <button onClick={signOut}>Sign out</button>
-            </div>
-          )}
-        </Authenticator>
         
         
         <p>
